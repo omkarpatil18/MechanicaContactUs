b888eb368be175c43b9abce208a96eb029b332e3
omkarpatil18 <indian.op45@gmail.com>
2017-01-27 11:55:56 +0530

    2nd

--- app/src/main/java/in/myvedik/android/contactus/ContactFragment.java
+++ app/src/main/java/in/myvedik/android/contactus/ContactFragment.java
@@ -6,12 +6,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.android.volley.toolbox.ImageLoader;
 
 /**
  * Created by admin on 10-01-2017.
  */
 
 public class ContactFragment extends Fragment{
+    private ImageView mSecy_1_ImageView;
+    private ImageView mSecy_2_ImageView;
+    private ImageView mJointSecyImageView;
+    private ImageView mEvents_1_ImageView;
+    private ImageView mEvents_2_ImageView;
+    private ImageView mStudentRelations_1_ImageView;
+    private ImageView mStudentRelations_2_ImageView;
+    private ImageView mSponsPr_1_ImageView;
+    private ImageView mSponsPr_2_ImageView;
+    private ImageView mWebMobOpsImageView;
+    private ImageView mDesignMediaImageView;
+    private ImageView mFinance_1_ImageView;
+    private ImageView mFinance_2_ImageView;
+    private ImageView mFacReqImageView;
+    private ImageView mQmsImageView;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,7 +40,41 @@ public class ContactFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.fragment_contact, container, false);
+
+        /*
+        mSecy_1_ImageView=v.findViewById(R.id.secy);
+        mSecy_1_ImageView.setTag(R.string.secy1_name);
+        mSecy_2_ImageView;
+        mJointSecyImageView;
+        mEvents_1_ImageView;
+        mEvents_2_ImageView;
+        mStudentRelations_1_ImageView;
+        mStudentRelations_2_ImageView;
+        mSponsPr_1_ImageView;
+        mSponsPr_2_ImageView;
+        mWebMobOpsImageView;
+        mDesignMediaImageView;
+        mFinance_1_ImageView;
+        mFinance_2_ImageView;
+        mFacReqImageView;
+        mQmsImageView;
+*/
+
+
+
+        return v;
+    }
+
+    private void setProfileImage(ImageView imageView){
+        ImageLoader mImageLoader = MySingleton.getInstance(getActivity()).getImageLoader();
+        mImageLoader.get(imageView.getTag().toString(), ImageLoader.getImageListener(imageView,
+                0, 0));
 
     }
+    private void buildUri(){
+
+    }
+
+
 }
